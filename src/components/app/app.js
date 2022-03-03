1710a88f377f1c32b377c34f58c9e7b82f6a8ee1
Yura Dubinin <yuradubinin100@gmail.com>
2022-03-03 19:58:20 +0700

    Initial version

---
+++ src/components/app/app.js
@@ -0,0 +1,31 @@
+import AppInfo from "../app-info/app-info";
+import SearchPanel from "../search-panel/search-panel";
+import AppFilter from "../app-filter/app-filter";
+import EmployeesList from "../employees-list/employees-list";
+import EmployeesAddForm from "../employees-add-form/employees-add-form";
+
+import "./app.css";
+
+function App() {
+    const data = [
+        { name: "John", salary: 800, increase: true, id: 1 },
+        { name: "Alex", salary: 3000, increase: false, id: 2 },
+        { name: "Carl", salary: 5000, increase: true, id: 3 },
+    ];
+
+    return (
+        <div className="app">
+            <AppInfo />
+
+            <div className="search-panel">
+                <SearchPanel />
+                <AppFilter />
+            </div>
+
+            <EmployeesList data={data} />
+            <EmployeesAddForm />
+        </div>
+    );
+}
+
+export default App;
